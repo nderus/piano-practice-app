50f045d836176f0641b2aff11d8fbf9b7c2335c9
nderus <nicolas.derus2@unibo.it>
2026-06-09 15:24:57 +0200

    Add configurable polyrhythm visualizer (Rhythm tab)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v13';
+const CACHE = 'piano-practice-v14';
 const ASSETS = [
   './',
   './index.html',
