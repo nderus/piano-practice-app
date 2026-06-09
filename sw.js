dc53c9ab7168d41e6fe4c3cf0debbd2d4a1fec65
nderus <nicolas.derus2@unibo.it>
2026-06-09 15:53:44 +0200

    Align visuals to heard audio time (output-latency compensation)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v16';
+const CACHE = 'piano-practice-v17';
 const ASSETS = [
   './',
   './index.html',
