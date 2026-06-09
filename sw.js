d93a3e3e7946a3c01c320a1d718bb453f2760aad
nderus <nicolas.derus2@unibo.it>
2026-06-09 15:02:15 +0200

    Add swinging pendulum to metronome

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v10';
+const CACHE = 'piano-practice-v11';
 const ASSETS = [
   './',
   './index.html',
