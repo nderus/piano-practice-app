745348740255c9317c7a0c2043078bb064703b48
nderus <nicolas.derus2@unibo.it>
2026-06-09 15:30:58 +0200

    Refine rhythm: editable tempo (default 52), softer clicks, stop metronome on tab switch, restore Metronome label

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v14';
+const CACHE = 'piano-practice-v15';
 const ASSETS = [
   './',
   './index.html',
