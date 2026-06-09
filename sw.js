b888ed4d494a0382446627ccabddd52a56551093
nderus <nicolas.derus2@unibo.it>
2026-06-09 16:32:33 +0200

    Move in-session pendulum to a faint background behind the name; secondary bar toggle

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v19';
+const CACHE = 'piano-practice-v20';
 const ASSETS = [
   './',
   './index.html',
