627456efcd04114d296f834ce426192340e1a1e4
nderus <nicolas.derus2@unibo.it>
2026-06-09 22:58:56 +0200

    Add chord-shape reference (keyboard lookup) to Chords tab; default metronome 60 BPM

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v26';
+const CACHE = 'piano-practice-v27';
 const ASSETS = [
   './',
   './index.html',
