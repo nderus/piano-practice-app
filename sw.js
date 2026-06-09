dedc91bcbb39e401b043d703e70097ecaa4643b0
nderus <nicolas.derus2@unibo.it>
2026-06-09 16:42:38 +0200

    Add Settings area with save/restore defaults; enlarge background pendulum to fill the card

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v20';
+const CACHE = 'piano-practice-v21';
 const ASSETS = [
   './',
   './index.html',
