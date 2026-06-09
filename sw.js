8972edebccf03d2e480f10d3f8d2489be4194204
nderus <nicolas.derus2@unibo.it>
2026-06-09 15:17:34 +0200

    Drive pendulum off audio clock to fix drift

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v12';
+const CACHE = 'piano-practice-v13';
 const ASSETS = [
   './',
   './index.html',
