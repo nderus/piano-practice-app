631ce1178a1297a9c671183d5bc7ddfa3e7300c5
nderus <nicolas.derus2@unibo.it>
2026-06-09 17:20:02 +0200

    Add About, Credits, Privacy and Share to Settings

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v23';
+const CACHE = 'piano-practice-v24';
 const ASSETS = [
   './',
   './index.html',
