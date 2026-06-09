fd42554744bcb2ff45cb3cbd5a8679b9bd682b65
nderus <nicolas.derus2@unibo.it>
2026-06-09 22:40:04 +0200

    Remove em-dashes from UI text

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v25';
+const CACHE = 'piano-practice-v26';
 const ASSETS = [
   './',
   './index.html',
