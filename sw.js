feecef4b5aa5b7493d98edd888299bdc01aa0d34
nderus <nicolas.derus2@unibo.it>
2026-06-10 02:10:03 +0200

    Fix metronome stall after timer throttling; surface Start buttons higher

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple offline cache for Piano Practice (PWA). Bump CACHE when files change. */
-const CACHE = 'piano-practice-v28';
+const CACHE = 'piano-practice-v29';
 const ASSETS = [
   './',
   './index.html',
